87ed6c3f4b97d433fddcddc9304935221d22fd32
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2025-09-13 17:25:19 -0700

    js part for total + count added

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ const heading = document.createElement("h2")
 heading.className = "text-2xl font-semibold pt-[10px] pb-[10px]"
 heading.innerText = "Your Cart (0 items)"
 cartBox.insertBefore(heading, sum)
+
+// total + count
+function updateTotal() {
+  let total = 0
+  cart.forEach(item => {
+    total += parseInt(item.price.replace("৳", ""))
+  })
+  sum.innerText = "Total: ৳" + total
+  heading.innerText = `Your Cart (${cart.length} items)`
+}
