52b30cd25ba804dfd1730dd2c25bbc3d4aa9f5e3
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2025-09-13 17:24:26 -0700

    js part for cart added

--- script.js
+++ script.js
@@ -2,3 +2,9 @@
 const cartBox = document.getElementById("yourCart")
 const sum = document.getElementById("total")
 const allclear = document.getElementById("clearCart")
+
+// cart
+const heading = document.createElement("h2")
+heading.className = "text-2xl font-semibold pt-[10px] pb-[10px]"
+heading.innerText = "Your Cart (0 items)"
+cartBox.insertBefore(heading, sum)
