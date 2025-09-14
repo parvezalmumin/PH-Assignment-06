166211c01702da5d578b1efe4a7ad018191cc7f6
Parvez Al Mumin <parvezalmuminofficial@gmail.com>
2025-09-13 17:26:26 -0700

    js part for add to cart added

--- script.js
+++ script.js
@@ -18,3 +18,32 @@ function updateTotal() {
   sum.innerText = "Total: ৳" + total
   heading.innerText = `Your Cart (${cart.length} items)`
 }
+
+// Add to cart
+function addToCart(nameId, priceId) {
+  const name = document.getElementById(nameId).innerText
+  const price = document.getElementById(priceId).innerText
+
+  cart.push({ name, price })
+
+  const item = document.createElement("div")
+  item.className = "flex justify-between items-center mt-2 bg-[#F9FAFB] p-2 rounded-md"
+
+  const itemText = document.createElement("span")
+  itemText.innerText = name + " - " + price
+
+  const cancelBtn = document.createElement("button")
+  cancelBtn.innerText = "❌"
+  cancelBtn.style.cursor = "pointer"
+  cancelBtn.addEventListener("click", function () {
+    cartBox.removeChild(item)
+    const index = cart.findIndex(i => i.name === name && i.price === price)
+    if (index > -1) cart.splice(index, 1)
+    updateTotal()
+  })
+
+  item.appendChild(itemText)
+  item.appendChild(cancelBtn)
+  cartBox.insertBefore(item, sum)
+  updateTotal()
+}
